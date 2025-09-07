4c3dc4b16c6b1456499dd2a0662544c0e006cd82
Stanislav Pankevich <s.pankevich@gmail.com>
2025-09-07 17:41:37 +0200

    Bump version to 0.0.22

--- html2pdf4doc/html2pdf4doc.py
+++ html2pdf4doc/html2pdf4doc.py
@@ -21,7 +21,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.core.os_manager import ChromeType, OperationSystemManager
 
-__version__ = "0.0.21"
+__version__ = "0.0.22"
 
 PATH_TO_HTML2PDF4DOC_JS = os.path.join(
     os.path.dirname(os.path.join(__file__)),
